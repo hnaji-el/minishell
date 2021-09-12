c8e1e4d357a1cc5ea69b12d9a7acff4283002152
Anas El kassimi <ael-kass@e1r2p11.1337.ma>
2021-09-12 10:46:50 +0100

    last version before start redirection

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -8,7 +8,7 @@ int		visitor_visit_redirection(t_redirect *node)
 	return (0);
 }
 
-int		visitor_visit_command(t_ast *node, t_node *head_env, int last_fd)
+int		visitor_visit_command(t_ast *node, t_node *head_env)
 {
 	int		i;
 	char	**cmd;
@@ -23,15 +23,23 @@ int		visitor_visit_command(t_ast *node, t_node *head_env, int last_fd)
 	i = 0;
 	while (i < node->args_size)
 	{
+		printf("%s\n", node->args_val[i]);
+		// write(1, node->args_val[i], 30);
 		cmd[i] = ft_strdup(node->args_val[i]);
 		i++;
 	}
 	cmd[i] = NULL;
-	start_exec(head_env, cmd, node->pipe_size, node, last_fd);
+	i = 0;
+	// while (cmd[i])
+	// {
+	// 	printf("%s\n", cmd[i]);
+	// 	i++;
+	// }
+	start_exec(head_env, cmd, node->pipe_size, node);
 	return (0);
 }
 
-int		visitor_visit_pipeline(t_ast *node, t_node *head_env, int last_fd)
+int		visitor_visit_pipeline(t_ast *node, t_node *head_env)
 {
 	int		i;
 	int		ret;
@@ -40,13 +48,13 @@ int		visitor_visit_pipeline(t_ast *node, t_node *head_env, int last_fd)
 	i = 0;
 	while (i < node->pipe_size)
 	{
-		ret = visitor_visit_command(node->pipe_val[i], head_env, last_fd);
+		ret = visitor_visit_command(node->pipe_val[i], head_env);
 		i++;
 	}
 	return (ret);
 }
 
-int		visitor_visit_compound(t_ast *node, t_node *head_env, int last_fd)
+int		visitor_visit_compound(t_ast *node, t_node *head_env)
 {
 	int		i;
 	int		ret;
@@ -56,19 +64,19 @@ int		visitor_visit_compound(t_ast *node, t_node *head_env, int last_fd)
 	while (i < node->comp_size)
 	{
 		// printf("---------compound------\n");
-		return (visitor_visit_pipeline(node->comp_val[i], head_env, last_fd));
+		return (visitor_visit_pipeline(node->comp_val[i], head_env));
 		i++;
 	}
 	return(ret);
 }
 
-int		visitor_visit(t_ast *node, t_node *head_env, int last_fd)
+int		visitor_visit(t_ast *node, t_node *head_env)
 {
 	int		ret;
 	
 	if (node == NULL)
 		return (258);
 	if (node->type == AST_COMPOUND)
-		ret = visitor_visit_compound(node, head_env, last_fd);
+		ret = visitor_visit_compound(node, head_env);
 	return (0);
 }
